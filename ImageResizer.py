ad6a7ad3eddfd73a47fae738825af72decb65451
jitesh1865 <jitzs.sharma55@gmail.com>
2024-06-13 23:44:52 +0530

    ImageResizer_file_V1.0

--- ImageResizer.py
+++ ImageResizer.py
@@ -1,8 +1,8 @@
 import cv2
 
-inimage="lotus.jpg"                  # take a image which we have to change as a input
-opimage="LotusNewImage1.png"         # Generated image name which we want at output
-scale_per=750    
+inimage="elonMuskIP.jpg"                  # take a image which we have to change as a input
+opimage="elonMuskNewImage1.png"         # Generated image name which we want at output image
+scale_per=850    
 
 
 img=cv2.imread(inimage,cv2.IMREAD_UNCHANGED)
@@ -19,6 +19,6 @@ dsize=(w,h) # tuple with width and height
 
 op=cv2.resize(img,dsize)   # resize() function is used here to resize the input image with new dimensions
 
-cv2.imwrite('LotusNewImage1.png',op)   # New modified generated image at output side
+cv2.imwrite('elonMuskNewImage1.png',op)   # New modified generated image at output side
 
 cv2.waitKey(0)  # pauses the script until a key is pressed (0 means waiting indefinitely)  Wait for the user to press a key
